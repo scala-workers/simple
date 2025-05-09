ef3d88465a5e4eb4fa2ab4500fcacda45ac48c6b
djx314 <djx314@sina.cn>
2025-05-10 03:00:26 +0800

    pythagoras.

--- wip/simple-codec/shared/src/main/scala/net/scalax/simple/codec/ReplaceByPropertyName.scala
+++ wip/simple-codec/shared/src/main/scala/net/scalax/simple/codec/ReplaceByPropertyName.scala
@@ -3,7 +3,11 @@ package net.scalax.simple.codec
 import net.scalax.simple.codec.to_list_generic.{BasedInstalled, SimpleProduct1}
 
 trait ReplaceByPropertyName[F[_[_]]] {
-  def replace[T[_]](proName: String, proValue: Any): F[T] => F[T]
+  def replaceImpl[UX](proName: String, proValue: UX): F[({ type X1[_] = UX })#X1] => F[({ type X1[_] = UX })#X1]
+  def replace[T[_]](proName: String, proValue: Any): F[T] => F[T] = {
+    val funcImpl: F[({ type X1[_] = Any })#X1] => F[({ type X1[_] = Any })#X1] = replaceImpl[Any](proName = proName, proValue = proValue)
+    funcImpl.asInstanceOf[F[T] => F[T]]
+  }
 }
 
 object ReplaceByPropertyName {
@@ -13,17 +17,14 @@ object ReplaceByPropertyName {
       indexOfPropertyName: IndexOfPropertyName[F],
       replaceByIndex: ReplaceByIndex[F],
       labelled: ModelLabelled[F]
-    ): ReplaceByPropertyName[F] =
-      new ReplaceByPropertyName[F] {
-        override def replace[T[_]](proName: String, proValue: Any): F[T] => F[T] = {
-          val indexInt = indexOfPropertyName.ofName(proName, labelled.modelLabelled)
-          replaceByIndex.replace(indexInt, proValue)
-        }
+    ): ReplaceByPropertyName[F] = new ReplaceByPropertyName[F] {
+      override def replaceImpl[UX](proName: String, proValue: UX): F[({ type X1[_] = UX })#X1] => F[({ type X1[_] = UX })#X1] = {
+        val indexInt = indexOfPropertyName.ofName(proName, labelled.modelLabelled)
+        replaceByIndex.replaceImpl[UX](indexInt, proValue)
       }
+    }
 
-    def derived(
-      basedInstalled: BasedInstalled[F]
-    ): ReplaceByPropertyName[F] = {
+    def derived(basedInstalled: BasedInstalled[F]): ReplaceByPropertyName[F] = {
       val appender1 = SimpleProduct1[F].derived(basedInstalled.basedInstalled)
       derivedImpl(IndexOfPropertyName[F].derived(appender1), ReplaceByIndex[F].derived(appender1), basedInstalled.labelled)
     }
