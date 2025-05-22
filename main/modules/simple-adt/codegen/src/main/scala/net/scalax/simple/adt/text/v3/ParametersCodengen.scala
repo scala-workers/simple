8fd41be9706914cc2ad3de6818e6145c4a9a3604
djx314 <djx314@sina.cn>
2025-05-22 22:12:18 +0800

    pythagoras.

--- main/modules/simple-adt/codegen/src/main/scala/net/scalax/simple/adt/text/v3/ParametersCodengen.scala
+++ main/modules/simple-adt/codegen/src/main/scala/net/scalax/simple/adt/text/v3/ParametersCodengen.scala
@@ -1,22 +1,20 @@
 package net.scalax.simple.adt.codegen
 
 class ParametersCodengen(val index: Int) {
-  ParametersCodengenSelf =>
 
   extension (list: Seq[String]) {
     def mkString(c: Char): String = list.mkString(c.toString)
   }
 
   class TraitDef(val index: Int) {
-    TraitDefSelf =>
 
     val typeParam = for (i1 <- 1 to index) yield s"T$i1"
 
     val text: String = s"def item$index[${typeParam.mkString(',')}]: M[${typeParam.mkString(',')}]"
+
   }
 
   class TraitBody(val index: Int) {
-    TraitBodySelf =>
 
     val traitDef: TraitDef = new TraitDef(index)
 
@@ -25,6 +23,7 @@ class ParametersCodengen(val index: Int) {
     val text: String = s"""trait ParameterNatSupport$index[M[${typeParam.mkString(',')}]] {
       ${traitDef.text}
     }"""
+
   }
 
   val preTextContent: Seq[String] = for (i <- 1 to index) yield new TraitBody(i).text
