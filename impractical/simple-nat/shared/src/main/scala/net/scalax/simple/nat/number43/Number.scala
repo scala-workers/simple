579709e2116e83d4ac3fc5a7064c559963277ab9
djx314 <djx314@sina.cn>
2025-12-25 06:03:08 +0800

    s

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number43/Number.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number43/Number.scala
@@ -15,37 +15,16 @@ object Num43 {
   }
 
   object Append1 {
-    val tailToRight: ghdmzsk = new ghdmzsk {
-      override def inputGHDMZSK(appendInnstance: () => ghdmzsk): ghdmzsk = new ghdmzsk {
-        override def inputGHDMZSK(tail1: () => ghdmzsk): ghdmzsk = new ghdmzsk {
-          override def inputGHDMZSK(num2: () => ghdmzsk): ghdmzsk = new ghdmzsk {
-            override def inputGHDMZSK(num3: () => ghdmzsk): ghdmzsk = new ghdmzsk {
-              override def inputGHDMZSK(num4: () => ghdmzsk): ghdmzsk = new ghdmzsk {
-                override def inputGHDMZSK(num5: () => ghdmzsk): ghdmzsk = new ghdmzsk {
-                  override def inputGHDMZSK(num6: () => ghdmzsk): ghdmzsk =
-                    appendInnstance().inputGHDMZSK(() =>
-                      num2().inputGHDMZSK(num3).inputGHDMZSK(num4).inputGHDMZSK(num5).inputGHDMZSK(num6).inputGHDMZSK(tail1)
-                    )
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-
-    val tailToRightInstance: ghdmzsk = Append1.tailToRight.inputGHDMZSK(() => Append2.tailToLeft).inputGHDMZSK(() => tailToRightInstance)
-
     val tailToLeft: ghdmzsk = new ghdmzsk {
       override def inputGHDMZSK(appendInnstance: () => ghdmzsk): ghdmzsk = new ghdmzsk {
-        override def inputGHDMZSK(tail1: () => ghdmzsk): ghdmzsk = new ghdmzsk {
+        override def inputGHDMZSK(num1: () => ghdmzsk): ghdmzsk = new ghdmzsk {
           override def inputGHDMZSK(num2: () => ghdmzsk): ghdmzsk = new ghdmzsk {
             override def inputGHDMZSK(num3: () => ghdmzsk): ghdmzsk = new ghdmzsk {
               override def inputGHDMZSK(num4: () => ghdmzsk): ghdmzsk = new ghdmzsk {
                 override def inputGHDMZSK(num5: () => ghdmzsk): ghdmzsk = new ghdmzsk {
                   override def inputGHDMZSK(num6: () => ghdmzsk): ghdmzsk =
                     appendInnstance().inputGHDMZSK(() =>
-                      num6().inputGHDMZSK(tail1).inputGHDMZSK(num2).inputGHDMZSK(num3).inputGHDMZSK(num4).inputGHDMZSK(num5)
+                      num6().inputGHDMZSK(num1).inputGHDMZSK(num2).inputGHDMZSK(num3).inputGHDMZSK(num4).inputGHDMZSK(num5)
                     )
                 }
               }
@@ -56,19 +35,40 @@ object Num43 {
     }
 
     val tailToLeftInstance: ghdmzsk = Append1.tailToLeft.inputGHDMZSK(() => Append2.tailToRight).inputGHDMZSK(() => tailToLeftInstance)
+
+    val tailToRight: ghdmzsk = new ghdmzsk {
+      override def inputGHDMZSK(appendInnstance: () => ghdmzsk): ghdmzsk = new ghdmzsk {
+        override def inputGHDMZSK(num1: () => ghdmzsk): ghdmzsk = new ghdmzsk {
+          override def inputGHDMZSK(num2: () => ghdmzsk): ghdmzsk = new ghdmzsk {
+            override def inputGHDMZSK(num3: () => ghdmzsk): ghdmzsk = new ghdmzsk {
+              override def inputGHDMZSK(num4: () => ghdmzsk): ghdmzsk = new ghdmzsk {
+                override def inputGHDMZSK(num5: () => ghdmzsk): ghdmzsk = new ghdmzsk {
+                  override def inputGHDMZSK(num6: () => ghdmzsk): ghdmzsk =
+                    appendInnstance().inputGHDMZSK(() =>
+                      num2().inputGHDMZSK(num3).inputGHDMZSK(num4).inputGHDMZSK(num5).inputGHDMZSK(num6).inputGHDMZSK(num1)
+                    )
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    val tailToRightInstance: ghdmzsk = Append1.tailToRight.inputGHDMZSK(() => Append2.tailToLeft).inputGHDMZSK(() => tailToRightInstance)
   }
 
   object Append2 {
     val tailToLeft: ghdmzsk = new ghdmzsk {
-      override def inputGHDMZSK(tail1: () => ghdmzsk): ghdmzsk = new ghdmzsk with Num2 {
-        override def pre2: ghdmzsk = tail1()
+      override def inputGHDMZSK(num1: () => ghdmzsk): ghdmzsk = new ghdmzsk with Num2 {
+        override def pre2: ghdmzsk = num1()
 
         override def inputGHDMZSK(num2: () => ghdmzsk): ghdmzsk = new ghdmzsk {
           override def inputGHDMZSK(num3: () => ghdmzsk): ghdmzsk = new ghdmzsk {
             override def inputGHDMZSK(num4: () => ghdmzsk): ghdmzsk = new ghdmzsk {
               override def inputGHDMZSK(num5: () => ghdmzsk): ghdmzsk = new ghdmzsk {
                 override def inputGHDMZSK(num6: () => ghdmzsk): ghdmzsk =
-                  num6().inputGHDMZSK(tail1).inputGHDMZSK(num2).inputGHDMZSK(num3).inputGHDMZSK(num4).inputGHDMZSK(num5)
+                  num6().inputGHDMZSK(num1).inputGHDMZSK(num2).inputGHDMZSK(num3).inputGHDMZSK(num4).inputGHDMZSK(num5)
               }
             }
           }
@@ -77,15 +77,15 @@ object Num43 {
     }
 
     val tailToRight: ghdmzsk = new ghdmzsk {
-      override def inputGHDMZSK(tail1: () => ghdmzsk): ghdmzsk = new ghdmzsk with Num1 {
-        override def pre1: ghdmzsk = tail1()
+      override def inputGHDMZSK(num1: () => ghdmzsk): ghdmzsk = new ghdmzsk with Num1 {
+        override def pre1: ghdmzsk = num1()
 
         override def inputGHDMZSK(num2: () => ghdmzsk): ghdmzsk = new ghdmzsk {
           override def inputGHDMZSK(num3: () => ghdmzsk): ghdmzsk = new ghdmzsk {
             override def inputGHDMZSK(num4: () => ghdmzsk): ghdmzsk = new ghdmzsk {
               override def inputGHDMZSK(num5: () => ghdmzsk): ghdmzsk = new ghdmzsk {
                 override def inputGHDMZSK(num6: () => ghdmzsk): ghdmzsk =
-                  num2().inputGHDMZSK(num3).inputGHDMZSK(num4).inputGHDMZSK(num5).inputGHDMZSK(num6).inputGHDMZSK(tail1)
+                  num2().inputGHDMZSK(num3).inputGHDMZSK(num4).inputGHDMZSK(num5).inputGHDMZSK(num6).inputGHDMZSK(num1)
               }
             }
           }
