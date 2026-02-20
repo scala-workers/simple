2a3c03728ea35d0e428fb31bf349638bd9352617
djx314 <djx314@sina.cn>
2026-02-21 05:24:23 +0800

    s

--- wip/simple-codec/simple-codec-circe/shared/src/main/scala/net/scalax/simple/codec/PlayJsonGeneric2.scala
+++ wip/simple-codec/simple-codec-circe/shared/src/main/scala/net/scalax/simple/codec/PlayJsonGeneric2.scala
@@ -4,7 +4,7 @@ package codec
 import play.api.libs.json._
 import play.api.libs.json.Reads._
 import play.api.libs.functional.syntax._
-import net.scalax.simple.adt.nat.support.{ABCFunc, SimpleProduct3 => SP3}
+import net.scalax.simple.adt.nat.support.{ABCFunc, SimpleProduct2 => SP2, SimpleProduct3 => SP3, SimpleProduct4 => SP4}
 
 object PlayJsonGeneric2 {
   type Named[_]  = String
@@ -128,5 +128,62 @@ object PlayJsonGeneric2 {
 
     Decoder.instance[F[cats.Id]](hCursor => contextWith(hCursor)(map1))
   }*/
+  private trait DecodeJson[Name, Dec, Model, DefaultValue] {
+    def fromJson(n: Name, enc: Dec, defVal: DefaultValue): JsResult[Model]
+  }
+
+  def decodeImpl[F[_[_]]](
+    sp2: SP2.ProductAdapter[F],
+    sp4: SP4.ProductAdapter[F],
+    named: F[Named],
+    g: () => F[Reads],
+    defaultValue: Option[F[({ type OptF[TU] = Option[() => TU] })#OptF]]
+  ): JsObject => JsResult[F[IdType]] = (hCursor: JsObject) => {
+    type OptF[TU]    = Option[() => TU]
+    type OptFGet[TU] = F[OptF] => OptF[TU]
+
+    val getField: GetFieldModel[F] = GetFieldModel[F].derived(sp2)
+
+    val appender: SP4.SimpleAppender[DecodeJson] = new SP4.SimpleAppender[DecodeJson] {
+      override def append[A1, A2, A3, A4, B1, B2, B3, B4, C1, C2, C3, C4](
+        cxF1: ABCFunc[A1, B1, C1],
+        cxF2: ABCFunc[A2, B2, C2],
+        cxF3: ABCFunc[A3, B3, C3],
+        cxF4: ABCFunc[A4, B4, C4]
+      )(
+        ma: DecodeJson[A1, A2, A3, A4],
+        mb: DecodeJson[B1, B2, B3, B4]
+      ): DecodeJson[C1, C2, C3, C4] = new DecodeJson[C1, C2, C3, C4] {
+        override def fromJson(n: C1, enc: C2, de: C4): JsResult[C3] = for {
+          t1 <- ma.fromJson(cxF1.takeHead(n), cxF2.takeHead(enc), cxF4.takeHead(de))
+          t2 <- mb.fromJson(cxF1.takeTail(n), cxF2.takeTail(enc), cxF4.takeTail(de))
+        } yield cxF3.append(t1, t2)
+      }
+      override def zero[N1, N2, N3, N4](n1: N1, n2: N2, n3: N3, n4: N4): DecodeJson[N1, N2, N3, N4] = new DecodeJson[N1, N2, N3, N4] {
+        override def fromJson(n: N1, enc: N2, de: N4): JsResult[N3] = JsSuccess(n3)
+      }
+    }
+
+    val typeGen: SP4.TypeGen[DecodeJson, Named, Reads, IdType, OptFGet] = ??? /*new SP4.TypeGen[DecodeJson, Named, Reads, IdType, OptFGet] {
+      override def gen[T]: DecodeJson[String, Reads[T], T, F[OptF] => Option[() => T]] =
+        new DecodeJson[String, Reads[T], T, F[OptF] => Option[() => T]] {
+          override def fromJson(n: String, dec: Reads[T], defVal: F[OptF] => Option[() => T]): JsResult[T] = {
+            val value1: JsResult[T] = hCursor.apply(n)
+            value1.fold[JsResult[T]](
+              (err: DecodingFailure) =>
+                defaultValue.fold[Decoder.Result[T]](Left(err))(r =>
+                  defVal(r).fold[Decoder.Result[T]](Left(err))(rValue1 => Right(rValue1()))
+                ),
+              rV => Right(rV)
+            )
+          }
+        }
+    }*/
+
+    val decoderFunc: DecodeJson[F[Named], F[Reads], F[IdType], F[OptFGet]] =
+      sp4.append[DecodeJson, Named, Reads, IdType, OptFGet](typeGen = typeGen, sAppender = appender)
+
+    decoderFunc.fromJson(named, g(), getField.getFieldModel[OptF])
+  }
 
 }
