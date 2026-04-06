cd09df3f192374c85d92d93f43fc270aeff007ea
djx314 <djx314@sina.cn>
2026-04-07 06:38:02 +0800

    s

--- main/test/test-common/shared/src/main/scala/net/scalax/simple/test/EitherRound.scala
+++ main/test/test-common/shared/src/main/scala/net/scalax/simple/test/EitherRound.scala
@@ -1,18 +1,15 @@
 package net.scalax.simple
 package test
 
-class TailSame[T](val head: T, val tail: () => TailSame[T])
+class TailSame[T](val head: T, val tail: () => TailSame[T]) {
+  def liftToLeft[R1]: LiftLeftMid[T, R1] =
+    new LiftLeftMid[T, R1](tail = () => new LiftLeft(left = head, tail = () => tail().liftToLeft[R1]))
+  def liftToRight[R1]: LiftRightMid[R1, T] =
+    new LiftRightMid[R1, T](tail = () => new LiftRight[R1, T](right = head, tail = () => tail().liftToRight[R1]))
+}
 
-class LeftRound[L, R](val left: L, val tail: () => RightRound[L, R]) {
-  def liftToLeft[L1, R1](l1: TailSame[L1]): LiftLeft[L1, R1] =
-    new LiftLeft[L1, R1](left = l1.head, tail = () => tail().liftToLeft[L1, R1](l1.tail()))
-  def liftToRight[L1, R1](l1: TailSame[R1]): LiftRight[L1, R1] =
-    new LiftRight[L1, R1](right = l1.head, tail = () => tail().liftToRight[L1, R1](l1.tail()))
-}
-class RightRound[L, R](val right: R, val tail: () => LeftRound[L, R]) {
-  def liftToLeft[L1, R1](l1: TailSame[L1]): LiftLeftMid[L1, R1]   = new LiftLeftMid[L1, R1](tail = () => tail().liftToLeft[L1, R1](l1))
-  def liftToRight[L1, R1](l1: TailSame[R1]): LiftRightMid[L1, R1] = new LiftRightMid[L1, R1](tail = () => tail().liftToRight[L1, R1](l1))
-}
+class LeftRound[L, R](val left: L, val tail: () => RightRound[L, R])
+class RightRound[L, R](val right: R, val tail: () => LeftRound[L, R])
 
 class LiftLeft[L, R](val left: L, val tail: () => LiftLeftMid[L, R]) {
   def other1(r: LiftRight[L, R]): LeftRound[L, R] = new LeftRound[L, R](left = left, tail = () => tail().other2(r))
