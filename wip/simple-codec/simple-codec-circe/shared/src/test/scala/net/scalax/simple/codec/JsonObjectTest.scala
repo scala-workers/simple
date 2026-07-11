a10f4731e7ff644bf22d09f41732d4fb7b28c8c9
djx314 <djx314@sina.cn>
2026-07-11 20:34:32 +0800

    s

--- wip/simple-codec/simple-codec-circe/shared/src/test/scala/net/scalax/simple/codec/JsonObjectTest.scala
+++ wip/simple-codec/simple-codec-circe/shared/src/test/scala/net/scalax/simple/codec/JsonObjectTest.scala
@@ -7,11 +7,12 @@ import net.scalax.simple.codec.to_list_generic.SingletonMap
 
 object JsonObjectTest {
 
-  object Value1
-  implicit def in1: SingletonMap[Value1.type] = SingletonMap.instance(Value1)
+  object Value1 {
+    implicit def in1: SingletonMap[Value1.type] = SingletonMap.instance(Value1)
+  }
 
   def main(str: Array[String]): Unit = {
-    Value1.asJson
+    println(Value1.asJson)
   }
 
 }
