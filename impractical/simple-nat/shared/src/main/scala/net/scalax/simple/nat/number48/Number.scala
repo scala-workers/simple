862b68e0e5712923a2afa30e2b436844cffb0f6a
djx314 <djx314@sina.cn>
2026-07-23 22:58:50 +0800

    s

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number48/Number.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number48/Number.scala
@@ -4,13 +4,15 @@ package number48
 
 object Number48 {
 
-  case class TimeResult(key: String, tail: () => TimeResult)
+  case class TimeTran(dizhi: Long, year: Long, month: Long, day: Long, hour: Long, min: Long)
+
+  case class TimeResult(key: TimeTran => TimeTran, tail: () => TimeResult)
 
   trait Time {
     def other(tail1: Time, tail2: Time, tail3: Time, tail4: Time, tail5: Time, tail6: Time): TimeResult
   }
 
-  case class TimeDong(key: String, tail1: () => Time) extends Time {
+  case class TimeDong(key: TimeTran => TimeTran, tail1: () => Time) extends Time {
     def other(tail2: Time, tail3: Time, tail4: Time, tail5: Time, tail6: Time, tail7: Time): TimeResult =
       TimeResult(key = key, tail = () => tail7.other(tail1(), tail2, tail3, tail4, tail5, tail6))
   }
