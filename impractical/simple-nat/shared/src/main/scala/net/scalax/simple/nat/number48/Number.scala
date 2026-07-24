9205d31c616a5b6b9f0dfe47dc372bff1f8b1bc4
djx314 <djx314@sina.cn>
2026-07-24 19:27:39 +0800

    s

--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number48/Number.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number48/Number.scala
@@ -4,22 +4,22 @@ package number48
 
 object Number48 {
 
-  case class TimeTran(dizhi: Long, year: Long, month: Long, day: Long, hour: Long, min: Long)
-
-  case class TimeResult(key: TimeTran => TimeTran, tail: () => TimeResult)
+  case class TimeResult(key: String, tail: () => TimeResult)
 
   trait Time {
-    def other(tail1: Time, tail2: Time, tail3: Time, tail4: Time, tail5: Time, tail6: Time): TimeResult
+    val other: (Time, Time, Time, Time, Time, Time) => TimeResult
   }
 
-  case class TimeDong(key: TimeTran => TimeTran, tail1: () => Time) extends Time {
-    def other(tail2: Time, tail3: Time, tail4: Time, tail5: Time, tail6: Time, tail7: Time): TimeResult =
-      TimeResult(key = key, tail = () => tail7.other(tail1(), tail2, tail3, tail4, tail5, tail6))
-  }
+  val TimeDong: (String, () => Time) => Time = (key: String, tail1: () => Time) =>
+    new Time {
+      override val other = (tail2: Time, tail3: Time, tail4: Time, tail5: Time, tail6: Time, tail7: Time) =>
+        TimeResult(key = key, tail = () => tail7.other(tail1(), tail2, tail3, tail4, tail5, tail6))
+    }
 
-  case class TimeCount(tail1: () => Time) extends Time {
-    def other(tail2: Time, tail3: Time, tail4: Time, tail5: Time, tail6: Time, tail7: Time): TimeResult =
-      tail2.other(tail3, tail4, tail5, tail6, tail7, tail1())
-  }
+  val TimeCount: (() => Time) => Time = (tail1: () => Time) =>
+    new Time {
+      override val other = (tail2: Time, tail3: Time, tail4: Time, tail5: Time, tail6: Time, tail7: Time) =>
+        tail2.other(tail3, tail4, tail5, tail6, tail7, tail1())
+    }
 
 }
--- impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number48/RunMain.scala
+++ impractical/simple-nat/shared/src/main/scala/net/scalax/simple/nat/number48/RunMain.scala
@@ -2,13 +2,13 @@ package net.scalax.simple
 package nat
 package number48
 
-import net.scalax.simple.nat.number48.Number48.{Time, TimeCount, TimeDong, TimeResult, TimeTran}
+import net.scalax.simple.nat.number48.Number48.{Time, TimeCount, TimeDong, TimeResult}
 
 import scala.annotation.tailrec
 
 object RunTest1 {
 
-  def build(current1: Long, name: TimeTran => TimeTran): Time = {
+  def build(current1: Long, name: String): Time = {
     def buildImpl(numLong: Long, zero: () => Time): Time = {
       if (numLong > 0) {
         TimeCount(() => buildImpl(numLong - 1, zero))
@@ -26,13 +26,12 @@ object RunTest1 {
   @tailrec
   def countImpl(
     num: () => TimeResult,
-    current: TimeTran,
+    current: Map[String, Long],
     printlnSum: Int,
     speed: Long,
-    dealResult: TimeTran => Unit
+    dealResult: Map[String, Long] => Unit
   ): Unit = {
-    val sum                  = current.dizhi + current.year + current.month + current.day + current.hour
-    val needPrintln: Boolean = sum % speed == 0
+    val needPrintln: Boolean = current.values.sum % speed == 0
 
     if (needPrintln) {
       dealResult(current): Unit
@@ -41,11 +40,12 @@ object RunTest1 {
     if (printlnSum > 0) {
       val currentNum = num()
 
-      val newCurrent: TimeTran = currentNum.key(current)
+      val oldValue: Long = current.getOrElse(currentNum.key, 1L)
+      val newMap         = current + (currentNum.key -> (oldValue + 1L))
 
       countImpl(
         currentNum.tail,
-        current = newCurrent,
+        current = newMap,
         printlnSum = if (needPrintln) printlnSum - 1 else printlnSum,
         speed = speed,
         dealResult = dealResult
@@ -57,11 +57,11 @@ object RunTest1 {
     num: () => TimeResult,
     printlnSum: Int,
     speed: Long = 80000,
-    dealResult: TimeTran => Unit
+    dealResult: Map[String, Long] => Unit
   ): Unit =
     countImpl(
       num = num,
-      current = TimeTran(1, 1, 1, 1, 1, 1),
+      current = Map.empty,
       printlnSum = printlnSum,
       speed = speed,
       dealResult = dealResult
@@ -69,33 +69,33 @@ object RunTest1 {
 
   def main(arr: Array[String]): Unit = {
     lazy val top: Time = TimeCount(() => top)
-    val 地支: Time       = build(12, s => s.copy(dizhi = s.dizhi + 1))
-    val 年: Time        = build(12, s => s.copy(year = s.year + 1))
-    val 月: Time        = build(30, s => s.copy(month = s.month + 1))
-    val 日: Time        = build(24, s => s.copy(day = s.day + 1))
-    val 时: Time        = build(60, s => s.copy(hour = s.hour + 1))
-    lazy val 分: Time   = TimeDong(s => s.copy(min = s.min + 1), () => 分)
+    val 地支: Time       = build(12, "dizhi")
+    val 年: Time        = build(12, "year")
+    val 月: Time        = build(30, "month")
+    val 日: Time        = build(24, "day")
+    val 时: Time        = build(60, "hour")
+    lazy val 分: Time   = TimeDong("min", () => 分)
 
     count(
       () => top.other(地支, 年, 月, 日, 时, 分),
       printlnSum = 50,
-      speed = 10000000,
+      speed = 100000000,
       dealResult = map => {
         println(map)
 
         println("min vs hour")
-        println(BigDecimal(map.min) / BigDecimal(map.hour))
+        println(BigDecimal(map.getOrElse("min", 1L)) / BigDecimal(map.getOrElse("hour", 1L)))
         println("hour vs day")
-        println(BigDecimal(map.hour) / BigDecimal(map.day))
+        println(BigDecimal(map.getOrElse("hour", 1L)) / BigDecimal(map.getOrElse("day", 1L)))
         println("day vs month")
-        println(BigDecimal(map.day) / BigDecimal(map.month))
-        println("month vs year")
-        println(BigDecimal(map.month) / BigDecimal(map.year))
+        println(BigDecimal(map.getOrElse("day", 1L)) / BigDecimal(map.getOrElse("month", 1L)))
+        println("day vs month")
+        println(BigDecimal(map.getOrElse("month", 1L)) / BigDecimal(map.getOrElse("year", 1L)))
 
         println("year vs day")
-        println(BigDecimal(map.day) / BigDecimal(map.year))
+        println(BigDecimal(map.getOrElse("day", 1L)) / BigDecimal(map.getOrElse("year", 1L)))
         println("day vs second")
-        println(BigDecimal(map.min * 60) / BigDecimal(map.day))
+        println(BigDecimal(map.getOrElse("min", 1L) * 60) / BigDecimal(map.getOrElse("day", 1L)))
 
         println("=== finished 1 ===")
       }
